a2bc90f11e75666c1e378062e5b4a311ebfafe71
Prakash Naidu <=>
2019-10-20 09:51:43 +0530

    testing

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({ status: 'connected' });
+  res.json({ status: 'Api endpoints' });
 });
 
 module.exports = router;
